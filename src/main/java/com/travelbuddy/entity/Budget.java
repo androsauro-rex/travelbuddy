485e584a4a32fb2a723a6b9463aa8745776f5e6c
Alessandro Fruci <ale.fruci@outlook.com>
2026-05-27 17:09:33 +0200

    Tutte le annotazioni messe; manca solo l'entity Destination

--- src/main/java/com/travelbuddy/entity/Budget.java
+++ src/main/java/com/travelbuddy/entity/Budget.java
@@ -26,12 +26,33 @@ public class Budget {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY) //auto_increment
-	private Integer id;
+	private Long id;
+	
 	@Column(nullable = false, precision = 10, scale = 2)
 	@NotNull(message = "Budget Pianificato Obbligatorio")
-	@Digits(fraction = 2, integer = 8, message = "Budget Pianificato non conforme al formato")
+	@Digits(fraction = 2, integer = 8, message = "Il Budget Pianificato da te inserito non ha un formato corretto")
 	@Positive(message = "Budget deve essere maggiore di 0")
 	private BigDecimal pianificato;
 	
+	@Column(nullable = false, name = "costo_volo_viaggio", precision = 10, scale = 2)
+	@NotNull(message = "costo volo viaggio obbligatorio")
+	@Digits(fraction = 2, integer = 8, message = "Il costo del volo da te inserito non ha un formato corretto")
+	@Positive(message = "Il costo del volo di un viaggio deve essere maggiore di 0")
+	private BigDecimal costoVoloViaggio; 
+	
+	@Column(name = "costo_pernottamento", precision = 10, scale = 2)
+	@Digits(fraction = 2, integer = 8, message = "Il costo del pernottamento da te inserito non ha un formato corretto")
+	@Positive(message = "Il costo del pernottamento deve essere maggiore di 0")
+	private BigDecimal costoPernottamento; 
+	
+	@Column(name = "spese_cibo", precision = 10, scale = 2)
+	@Digits(fraction = 2, integer = 8, message = "Il costo speso per il cibo da te inserito non ha un formato corretto")
+	@Positive(message = "Il costo speso per il cibo deve essere maggiore di 0")
+	private BigDecimal cibo; 
+	
+	@Column(name = "spese_extra", precision = 10, scale = 2)
+	@Digits(fraction = 2, integer = 8, message = "Il costo delle spese extra da te inserito non ha un formato corretto")
+	@Positive(message = "Il costo delle spese extra deve essere maggiore di 0")
+	private BigDecimal extra; 
 	
 }
--- src/main/java/com/travelbuddy/entity/Giorno.java
+++ src/main/java/com/travelbuddy/entity/Giorno.java
@@ -2,11 +2,15 @@ package com.travelbuddy.entity;
 
 import java.time.LocalDate;
 
+import org.springframework.format.annotation.DateTimeFormat;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
@@ -24,9 +28,15 @@ public class Giorno {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY) //auto_increment
-	private Integer id;
+	private Long id;
+	
 	@Column(nullable = false)
 	@NotNull(message = "Data Obbligatoria")
+	@DateTimeFormat(iso = DateTimeFormat.ISO.DATE)	
 	private LocalDate data;
+	
+	@ManyToOne
+	@JoinColumn(name = "idItinerario")
+	private Itinerario itinerario; 
 
 }
