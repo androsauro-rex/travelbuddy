b9af6e6f7dde00f42a0d66584ab6e30f06808750
Andrea Lamicela <andrealamicela9996@gmail.com>
2026-05-27 17:38:35 +0200

    a

--- src/main/java/com/travelbuddy/entity/Utente.java
+++ src/main/java/com/travelbuddy/entity/Utente.java
@@ -12,6 +12,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -29,16 +30,22 @@ public class Utente {
 	@GeneratedValue(strategy = GenerationType.IDENTITY) //auto_increment
 	private Integer id;
 	@Column(nullable = false, length = 128)
+	@NotNull(message = "Nome Obbligatorio")
 	private String nome;
 	@Column(nullable = false, length = 128)
+	@NotNull(message = "Cognome Obbligatorio")
 	private String cognome;
 	@Column(nullable = false, length = 128, unique = true)
+	@NotNull(message = "Nickname Obbligatorio")
 	private String nickname;
 	@Column(nullable = false, length = 128)
+	@NotNull(message = "Password Obbligatoria")
 	private String password;
 	@Column(nullable = false, length = 128, unique = true)
+	@NotNull(message = "Email Obbligatorio")
 	private String email;
 	@Column(nullable = false, length = 4)
+	@NotNull(message = "Età Obbligatoria")
 	private Integer eta;
 	@Column(nullable = false)
 	@Enumerated(EnumType.STRING)
--- src/main/java/com/travelbuddy/listaenum/EnumStatus.java
+++ src/main/java/com/travelbuddy/listaenum/EnumStatus.java
@@ -1,9 +1,9 @@
 package com.travelbuddy.listaenum;
 
 public enum EnumStatus {
-	
+
 	ATTIVO,
 	DISATTIVO,
 	BANNATO
-
+	
 }
