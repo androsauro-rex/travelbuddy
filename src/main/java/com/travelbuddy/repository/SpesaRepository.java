4f5a98b5f8404e44da455d37fb2c8b140ac69f81
Alessandro Fruci <ale.fruci@outlook.com>
2026-05-29 12:59:35 +0200

    Aggiunto metodi in SpeseRepository e ItinerarioRepository per le spese

--- src/main/java/com/travelbuddy/repository/SpesaRepository.java
+++ src/main/java/com/travelbuddy/repository/SpesaRepository.java
@@ -1,9 +1,17 @@
 package com.travelbuddy.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.travelbuddy.entity.Spesa;
 
 public interface SpesaRepository extends JpaRepository<Spesa, Long>{
+	
+	@Query("SELECT s FROM Spesa s JOIN Itinerario i WHERE i.id = :idItinerario")
+	List<Spesa> findAllSpeseByItinerarioId(@Param("idItinerario") Long idItinerario);
+	
 	
 }
