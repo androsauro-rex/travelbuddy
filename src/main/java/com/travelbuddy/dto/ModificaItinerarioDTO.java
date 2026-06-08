fb1086ad6536d1f9864752de32ea23b989198fa1
Andrea Lamicela <andrealamicela9996@gmail.com>
2026-06-08 20:52:42 +0200

    Modifiche DTO, Controller, Service

--- src/main/java/com/travelbuddy/dto/ModificaItinerarioDTO.java
+++ src/main/java/com/travelbuddy/dto/ModificaItinerarioDTO.java
@@ -2,18 +2,28 @@ package com.travelbuddy.dto;
 
 import java.util.List;
 
-import com.travelbuddy.entity.Giorno;
-
-import lombok.AllArgsConstructor;
+import jakarta.validation.Valid;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+/**
+ * Versione corretta:
+ *  - usa List<GiornoDTO> (NON List<Giorno> entity)
+ *  - @NoArgsConstructor + campi non-final, per Jackson
+ *
+ * Così il frontend manda i giorni come JSON pulito, e ogni giorno
+ * porta dentro le sue tappe.
+ */
 @Getter
 @Setter
-@AllArgsConstructor
+@NoArgsConstructor
 public class ModificaItinerarioDTO {
-	
-	private final ItinerarioUpdateDTO itinerarioDTO; 
-	private final List<Giorno> giornoDTO; 
-	
+
+	@Valid
+	private ItinerarioUpdateDTO itinerarioDTO;
+
+	@Valid
+	private List<GiornoDTO> giornoDTO;
+
 }
